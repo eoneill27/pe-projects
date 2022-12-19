1f8242ced26e7a84a4ce57d6f9f5dfd3128366b9
Emily <oneill.emily@gmail.com>
2022-12-19 12:42:03 -0800

    Update portfolio site footer links

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 		</main>
 
 		<footer class="site-footer">
-			<div class="inner-column">
-				
-			</div>
+			<inner-column>
+				<a class="quiet-voice" href="https://github.com/eoneill27" target="_blank">GitHub</a>
+				<a class="quiet-voice" href="https://oneillemily.substack.com/" target="_blank">Substack</a>
+				<a class="quiet-voice" href="https://codepen.io/eoneill27" target="_blank">CodePen</a>
+			</inner-column>
 
 		</footer>
 
