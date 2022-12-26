d88721dce4f7056fc1ab20dcf81c9373e32334e5
Emily <oneill.emily@gmail.com>
2022-12-26 12:17:32 -0800

    Update personal portfolio About page

--- about.php
+++ about.php
@@ -4,4 +4,18 @@
 	<h1 class="heyyou-voice">About Emily</h1>
 </section>
 
+<section class="hello">
+	<inner-column>
+		<div class="hello-text">
+			<p class="reading-voice">I grew up in Shoreline, Washington, but I have made Vancouver, British Columbia my home for the past 18 years. I am so lucky to live in Cascadia - I am happiest when I'm outdoors, and there are few places in the world with more natural beauty than B.C. In my spare time, I love to walk long distances, swim in lakes, read in pubs, solve crossword puzzles, play with my friends' kids, and camp in my car.</p>
+
+			<p class="reading-voice">After graduating with a history degree from the University of North Carolina, I made my way to Vancouver for library school at the University of British Columbia. I had an interest in archival work but ended up getting a degree in both archival and library studies. Life took me down the library path, and I spent 14 years working as a librarian for the provincial teachers' union. At a certain point, I realized that I needed a change. I wasn't finding my work inspiring or challenging, so I started looking around for something different.</p>
+
+			<p class="reading-voice">Enter the mentorship program offered by Perpetual Education. I was thrilled to start Design for the Web in September, 2022 (while on holiday in Morocco!), and I expect to finish the course in April, 2023.</p>
+
+			<p class="reading-voice">I bring a strong background in information retrieval and organization to my web development work, and I create accessible, responsive websites. I have a strong attention to detail, excellent communication skills, a doggedness when it comes to problem-solving, and a commitment to honest, up-front business dealings. I look forward to meeting you and finding out about your project!</p>
+		</div>
+	</inner-column>
+</section>
+
 <?php include("footer.php"); ?>
