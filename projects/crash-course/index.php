607664f8a66d289d8bff691451280907b9effd62
Emily <oneill.emily@gmail.com>
2023-01-06 07:02:18 -0800

    Update crash course
    Dark mode test

--- projects/crash-course/index.php
+++ projects/crash-course/index.php
@@ -14,12 +14,18 @@
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 
-	<body>
+	<?php if(isset($_GET['mode'])) {
+						echo "<body class='dark-mode'>"; 
+					} else echo "<body>";
+			?>
 
 		<header class="site-header">
 			<a href="https://www.pbs.org" class="logo">PBS</a>
 			<!-- <span>&#124;</span> -->
 
+			<a href="http://peprojects.dev/alpha-6/emily/projects/crash-course/?mode=dark" class="dark-mode">dark</a>/<a href="http://peprojects.dev/alpha-6/emily/projects/crash-course">light</a>
+
+			
 			
 		</header>
 
@@ -44,6 +50,70 @@
 		<footer class="site-footer">
 			<section class="top-footer">
 				<inner-column>
+					<div class="footer1">
+						<h1>LOGO</h1>
+					</div>
+					<div class="footer2">
+						<div class="footer2-1">
+							<h2>Footer Heading</h2>
+								<ul class="footer-list">
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+								</ul>
+						</div>
+						<div class="footer2-2">
+							<ul>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+							</ul>
+						</div>
+					</div>
+					<div class="footer3">
+						<div class="footer3-1">
+							<h2>Footer Heading</h2>
+								<ul class="footer-list">
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+								</ul>
+						</div>
+						<div class="footer3-2">
+							<ul>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+							</ul>
+						</div>
+					</div>
+					<div class="footer4">
+						<div class="footer4-1">
+							<h2>Footer Heading</h2>
+								<ul class="footer-list">
+									<li class="footer-list-item">List item</li>
+									<li class="footer-list-item">List item</li>
+								</ul>
+						</div>
+						<div class="footer4-2">
+							<ul>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+								<li class="footer-list-item">List item</li>
+							</ul>
+						</div>
+					</div>
+					<div class="footer5">
+						<h2>SVG goes here</h2>
+					</div>
 
 				</inner-column>
 			</section>
