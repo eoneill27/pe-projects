1f7e219168f026ec498da43a00809be17d2abbeb
Emily <oneill.emily@gmail.com>
2023-01-07 21:23:53 -0800

    Update portfolio site
    Reorganization of files - created index files for subpages (except Projects)

---
+++ resume/index.php
@@ -0,0 +1,64 @@
+<?php include('header.php'); ?>	
+
+<inner-column>
+	<section class="resume">
+		<h1 class="attention-voice">Resume</h1>
+
+		<h2 class="info-voice">Education</h2>
+		<ul class="reading-voice">
+			<li>Perpetual Education, Design for the Web - September 2022-present</li>
+			<li>University of British Columbia - Master of Library and Information Studies and Master of Archival Studies, joint degree - 2005-2008</li>
+			<li>University of North Carolina at Chapel Hill, BA in History - 2001-2005</li>
+		</ul>
+		
+		<h2 class="info-voice">Work Experience</h2>
+		<ul class="reading-voice">
+			<li><h3>Information Specialist</h3>
+				September 2009-present<br/>
+				British Columbia Teachers' Federation<br/>
+				Vancouver, British Columbia<br/>
+			</li>
+			<li><h3>Librarian</h3>
+				July 2008-June 2009<br/>
+				Sandwell Engineering, Inc.<br/>
+				Vancouver, British Columbia<br/>
+			</li>
+			<li><h3>Graduate Academic Assistant, Reference Desk</h3>
+				September 2007-April 2008<br/>
+				Woodward Biomedical Library, University of British Columbia<br/>
+				Vancouver, British Columbia<br/>
+			</li>
+			<li><h3>Student Library Assistant</h3>
+				May 2007-April 2008<br/>
+				Woodward Biomedical Library, University of British Columbia<br/>
+				Vancouver, British Columbia<br/>
+			</li>
+			<li><h3>Library Assistant</h3>
+				October 2006-May 2007<br/>
+				Disability Services, University of British Columbia<br/>
+				Vancouver, British Columbia<br/>
+			</li>
+			<li><h3>Manuscripts Processing Assistant</h3>
+				August 2002-May 2005<br/>
+				Manuscripts Department, Academic Affairs Library<br/>
+				University of North Carolina, Chapel Hill<br/>
+			</li>
+
+		</ul>
+
+		<h2 class="info-voice">Personal Activities and Interests</h2>
+		<ul class="reading-voice">
+			<li>Coach for the Canadian Girls Baseball development league since 2020</li>
+			<li>Ten-time Canadian champion at the American Crossword Puzzle Tournament</li>
+			<li>Executive member of the Teachers' Federation Employees' Union since 2015 - currently in the role of Secretary. Served as Vice President from 2018-2021</li>
+			<li>Level 2 occupational first aid attendant since 2014</li>
+			<li>Member of two recreational softball teams - one orthodox and one very, very slo-pitch</li>
+			<li>Member of a ukulele trio (but don't ask - we only perform for our book club)</li>
+			<li>Templeton Swim Boot Camp team member since 2015</li>
+			<li>UBC sprint triathlon finisher seven years running</li>
+		</ul>
+		
+	</section>
+</inner-column>
+
+<?php include('footer.php'); ?>
