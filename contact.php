7c103298a9772a1339e5f34485110a8c597f0c17
Emily <oneill.emily@gmail.com>
2023-01-07 21:35:57 -0800

    Update portfolio site
    Revert back to subpages at root

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php include('header.php'); ?>	
+
+<inner-column>
+	<section class="contact" id="contact">
+		<h1 class="attention-voice">Contact Emily</h1>
+		<div class="contact-text">
+			<span class="reading-voice">You can always reach me at
+			<address class="reading-voice"><a href="mailto:oneill.emily@gmail.com">oneill.emily@gmail.com</a></address></span>
+			<p class="reading-voice">I'll do my very best to get back to you within 24 hours. I'm looking forward to working with you!</p>
+		</div>
+	</section>
+</inner-column>
+
+<?php include('footer.php'); ?>
