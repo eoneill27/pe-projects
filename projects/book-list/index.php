726c8b997365aff448f6a3fcf2b2ddf91a674d18
Emily <oneill.emily@gmail.com>
2023-01-11 18:58:35 -0800

    Update book list with home page form
    Mostly for practice - but might work somewhere down the line

--- projects/book-list/index.php
+++ projects/book-list/index.php
@@ -1,5 +1,75 @@
 <?php include('header.php'); ?>
 
+<?php
+
+$title = "";
+$author = "";
+$genre = "";
+$year = "";
+$recommend = null;
+
+if (isset($_POST["submitted"])) {
+	if (isset($_POST["title"])) {
+		$title = $_POST["title"];
+	}
+	if (isset($_POST["author"])) {
+		$author = $_POST["author"];
+	}
+	if (isset($_POST["genre"])) {
+		$genre = $_POST["genre"];
+	}
+	if (isset($_POST["year"])) {
+		$year = $_POST["year"];
+	}
+	if (isset($_POST["title"])) {
+		$title = $_POST["title"];
+	}
+	if (isset($_POST["recommend"])) {
+		$recommend = true;
+	}
+}
+
+$bookArray = [$title, $author, $genre, $year, $recommend];
+
+?>
+
+<inner-column>
+
+	<form method="POST" action="">
+
+		<p class="title-voice">What book did you just finish?</p>
+
+		<div class="form-fields author-voice">
+
+			<label for="">Title</label>
+			<input name="title" type="text" size="40ch"/>
+
+			<label for="">Author</label>
+			<input name="author" type="text" size="30ch" />
+
+			<label for="">Genre</label>
+			<input name="genre" type="text" size="30ch" />
+
+			<label for="">Year read</label>
+			<input name="year" type="text" max="4" />
+
+			<div class="recommend-check">
+				<legend>Do you recommend this book?</legend>
+				<input name="recommend" type="checkbox" id="yes" value="yes" />
+				<label for="yes">Yes</label>
+			</div>
+
+			<button name="submitted" type="submit" class="author-voice">
+				Submit
+			</button>
+
+		</div>
+	</form>
+
+<p class="author-voice output-array">Here's what you just entered:<br/> <span class="genre-voice"><?=var_dump($bookArray)?> </span></p>
+
+</inner-column>
+
 
 
 
