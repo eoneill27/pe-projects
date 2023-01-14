cabd1b1d56604b51d7bfb81cbc7df2fcf7845607
Emily <oneill.emily@gmail.com>
2023-01-14 09:03:42 -0800

    Update e4p
    Created forms data, foreach loop, grid display

---
+++ projects/e4p/data/forms.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$currency = [
+	"title" => "Currency Converter",
+	"description" => "A form that will convert your money from one currency to another.",
+	"url" => "currency-converter.php",
+	"img" => "https://peprojects.dev/images/landscape.jpg"
+];
+
+$retirement = [
+	"title" => "Retirement Calculator",
+	"description" => "This form will calculate the year of your retirement and how many years you have left to work.",
+	"url" => "retirement-calculator.php",
+	"img" => "https://peprojects.dev/images/landscape.jpg"
+];
+
+$math = [
+	"title" => "Simple Math",
+	"description" => "This is a simple calculator - enter two numbers, and four different mathematical operations will be performed for you.",
+	"url" => "simple-math.php",
+	"img" => "https://peprojects.dev/images/landscape.jpg"
+];
+
+$tip = [
+	"title" => "Tip Calculator",
+	"description" => "This form allows you to enter the subtotal of your bill and add a tip rate. It will output your tip and your total bill.",
+	"url" => "tip-calculator.php",
+	"img" => "https://peprojects.dev/images/landscape.jpg"
+];
+
+$forms = [$currency, $retirement, $math, $tip];
+
+?>
+
+
--- projects/e4p/index.php
+++ projects/e4p/index.php
@@ -36,85 +36,47 @@
 		</header>
 
 		<main class="page-content">
+			<section class="e4p">
 
-<inner-column>
-	
-	<h1 class="heyyou-voice">Exercises for Programmers</h1>
-	<h2 class="info-voice">A collection of forms based on <a href="https://pragprog.com/titles/bhwb/exercises-for-programmers/" target="_blank">Brian P. Hogan's book</a></h2>
+				<inner-column>
+					
+					<h1 class="heyyou-voice">Exercises for Programmers</h1>
+					<h2 class="info-voice">A collection of forms based on <a class="book-link" href="https://pragprog.com/titles/bhwb/exercises-for-programmers/" target="_blank">Brian P. Hogan's book</a></h2>
 
+					<div class="form-grid">
 
-	<details>
-		<summary><h3 class="reading-voice">Simple Math</h3></summary>
-		<p class="nav-voice">This is a simple calculator - enter two numbers, and four different mathematical operations will be performed for you.</p>
-		<?php
+						<?php include("data/forms.php")?>
+						
+						<ul>
 
-$number1 = 0;
-$number2 = 0;
+							<?php
 
-if (isset($_POST["submitted"])) {
+							foreach ($forms as $form) {
 
-	if (isset($_POST["number1"])) {
-		$number1 = $_POST["number1"];
-	}
+								$title = $form["title"];
+								$description = $form["description"];
+								$url = $form["url"];
+								$img = $form["img"];
 
-	if (isset($_POST["number2"])) {
-		$number2 = $_POST["number2"];
-	}
-}
+							?>
 
-$add = $number1 + $number2;
+								<li class="form" style='list-style-type: none'>
+									<a href="<?=$url?>">
+										<picture class="form-tile">
+											<img src="<?=$img?>" alt="">
+										</picture>
+									</a>
+									<h3 class="info-voice"><?=$title?></h3>
+									<p class="reading-voice"><?=$description?></p>
+								</li>
 
-$subtract = $number1 - $number2;
+							<?php } ?>
 
-$multiply = $number1 * $number2;
+						</ul>
 
-if ($number2 == 0) {
-	$divide = NULL;
-	$validateDivide = "You can't divide by 0. Please enter a number other than 0.";
-} else {
-	$divide = $number1 / $number2;
-	$validateDivide = NULL;
-}
-
-
-?>
-
-<a href="?"><h1>Simple calculator</h1></a>
-
-<form method="POST" action="">
-	<div class="form-fields reading-voice">
-		<label for="">Number 1</label>
-		<input name="number1" type="number" value="<?=$number1?>" step="0.0000001"/>
-
-		<label for="">Number 2</label>
-		<input name="number2" type="number" value="<?=$number2?>" step="0.0000001"/>
-
-		<button name="submitted" type="submit">
-			Submit
-		</button>
-	</div>
-
-	<div class="form-output reading-voice">
-		<h2 class="attention-voice">RESULTS:</h2>
-		<p>Number 1 + Number 2 = <?=$add?></p>
-		<p>Number 1 - Number 2 = <?=$subtract?></p>
-		<p>Number 1 * Number 2 = <?=$multiply?></p>
-		<p>Number 1 / Number 2 = <?=$divide?></p>
-		<p><?=$validateDivide?></p>
-
-	</div>
-
-
-
-</form>
-
-
-	</details>
-
-</inner-column>
-
-
-
-
+					</div>
+					
+				</inner-column>
+			</section>
 
 <?php include("../../footer.php"); ?>
