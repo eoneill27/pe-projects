0a3d9a769e73d5b65ef657e4477eecb68433ea71
Emily <oneill.emily@gmail.com>
2023-01-16 18:09:14 -0800

    Update book club
    Added next meeting info to home page

--- projects/book-club/home.php
+++ projects/book-club/home.php
@@ -1,4 +1,11 @@
 <h1 class="heyyou-voice">Poison Raisins Forever!</h1>
 
+<div class="next-meeting">
+	<h2 class="attention-voice">NEXT MEETING:</h2>
+	<p class="info-voice">February 3, 2023 at Allison's</p>
+
+	<h2 class="attention-voice">NEXT BOOK:</h2>
+	<p class="info-voice">To Be or Not to Be/Ryan North</p>
+</div>
 
 
