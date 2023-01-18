834adc4430db836fd3ba790d1b5f2adf7a610578
Emily <oneill.emily@gmail.com>
2023-01-18 07:44:55 -0800

    Update book club
    Add addMeeting.php;
    Update meeting grid dimensions

---
+++ projects/book-club/addMeeting.php
@@ -0,0 +1,35 @@
+<h1 class="heyyou-voice">Add a meeting</h1>
+
+<?php
+
+	if (isset($_POST["add"])) {
+		$message = "The meeting has been added!";
+	} else {
+		$message = NULL;
+	}
+
+?>
+
+<form action="" method="POST">
+	
+	<field>
+
+	</field>
+
+	<field>
+
+	</field>
+
+	<field>
+
+	</field>
+
+	<field>
+
+	</field>
+
+
+	<button type="submit" name="add">
+		
+	</button>
+</form>
