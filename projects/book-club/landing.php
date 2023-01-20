5c398915b9b967978bca91a73f6f8c791f87d983
Emily <oneill.emily@gmail.com>
2023-01-20 06:20:46 -0800

    Update book club

--- projects/book-club/landing.php
+++ projects/book-club/landing.php
@@ -5,7 +5,7 @@
 		background-image: url(images/poison-raisins.svg);
 		background-repeat: no-repeat;
 		background-size: 150%;
-		background-position: center 5px;
+		background-position: center 2px;
 	}
 
 	@media (min-width: 500px) {
