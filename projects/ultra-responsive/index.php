7d010ef115db97bdad128bc6170e62a0cb6c791a
Emily <oneill.emily@gmail.com>
2023-01-23 07:47:52 -0800

    Initial commit super ultra responsive layout challenge
    Completed masthead module

---
+++ projects/ultra-responsive/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Super Ultra Responsive Layout Challenge</title>
+		<meta name="description" content="[[insert description]]">
+
+		<meta property="og: image" content="[[meta image source]]">
+		<link rel="icon" href="[[insert favicon source]]">
+
+		<link rel="stylesheet" href="css/site.css">
+	</head>
+
+	<body>
+
+		<header>
+			<inner-column>
+				
+				<?php include("masthead.php"); ?>
+
+			</inner-column>
+		</header>
+
+
+	</body>
+
+</html>
---
+++ projects/ultra-responsive/masthead.php
@@ -0,0 +1,17 @@
+<head>
+	<link rel="stylesheet" href="css/masthead.css">
+</head>
+
+<mast-head>
+
+	<nav class="site-menu">
+		<a href="?">LOGO</a>
+		<a href="?">Some page</a>
+		<a href="?">"Masthead"</a>
+	</nav>
+
+	<nav class="user-menu">
+		<a href="?">Sign in</a>
+	</nav>
+
+</mast-head>
