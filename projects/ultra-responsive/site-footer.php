f66aa6b41eb9b913b6b50f3402dd8c9e88a38772
Emily <oneill.emily@gmail.com>
2023-01-23 18:55:02 -0800

    Site-footer module

---
+++ projects/ultra-responsive/site-footer.php
@@ -0,0 +1,35 @@
+<head>
+	<link rel="stylesheet" href="css/site-footer.css">
+</head>
+
+<site-footer>
+	
+	<div class="footer-header">
+		<h2 class="heyyou-voice">This is the site footer and it has a "site-map" module in it.</h2>
+		<p class="reading-voice">Pretty standard type of thing, right?</p>
+	</div>
+
+	<div class="footer-links">
+		<nav class="site-menu">
+			<a class="moving-voice" href="?">LOGO</a>
+			<a class="moving-voice" href="?">Some page</a>
+			<a class="moving-voice" href="?">"Masthead"</a>
+		</nav>
+
+		<nav class="user-menu">
+			<a class="moving-voice" href="?">Sign in</a>
+		</nav>
+
+		<nav class="legal-menu">
+			<a href="?" class="moving-voice">Legal thing a</a>
+			<a href="?" class="moving-voice">Legal thing b</a>
+		</nav>
+
+		<nav class="social-menu">
+			<a href="?" class="moving-voice">social thing a</a>
+			<a href="?" class="moving-voice">social thing b</a>
+			<a href="?" class="moving-voice">social thing c</a>
+			<a href="?" class="moving-voice">social thing d</a>
+		</nav>
+	</div>
+</site-footer>
