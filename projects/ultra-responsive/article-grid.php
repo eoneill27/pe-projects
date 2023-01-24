c3fef257ddec268ce18303be6db4a62ff815a962
Emily <oneill.emily@gmail.com>
2023-01-23 18:32:39 -0800

    Article grid module

--- projects/ultra-responsive/article-grid.php
+++ projects/ultra-responsive/article-grid.php
@@ -1,1 +1,70 @@
-article-grid.php
+<head>
+	<link rel="stylesheet" href="css/article-grid.css">
+</head>
+
+<article-grid>
+
+	<div class="article-heading">
+		<h2 class="heyyou-voice">This is an "Article Grid" module. This is its heading.</h2>
+
+		<p class="reading-voice">Again, we totally just made that up. It's a box with boxes in it.</p>
+	</div>
+
+	<div class="article-cards">
+		
+		<div class="article-card-act">
+			<h3 class="heyyou-voice">This is an article card</h3>
+			<p class="reading-voice">It's basically a mini call to action, right?</p>
+
+			<div class="site-button">
+				<a href="">Act!</a>
+			</div>
+		</div>
+
+		<div class="article-card-read">
+			<h3 class="heyyou-voice">This is the title of an article</h3>
+			<p class="reading-voice">Here's a little info to help you understand if this is an article you want to read.</p>
+
+			<div class="site-button">
+				<a href="">Read more</a>
+			</div>
+		</div>
+
+		<div class="article-card-read">
+			<h3 class="heyyou-voice">This is the title of an article</h3>
+			<p class="reading-voice">Here's a little info to help you understand if this is an article you want to read.</p>
+
+			<div class="site-button">
+				<a href="">Read more</a>
+			</div>
+		</div>
+
+		<div class="article-card-read">
+			<h3 class="heyyou-voice">This is the title of an article</h3>
+			<p class="reading-voice">Here's a little info to help you understand if this is an article you want to read.</p>
+
+			<div class="site-button">
+				<a href="">Read more</a>
+			</div>
+		</div>
+
+		<div class="article-card-read">
+			<h3 class="heyyou-voice">This is the title of an article</h3>
+			<p class="reading-voice">Here's a little info to help you understand if this is an article you want to read.</p>
+
+			<div class="site-button">
+				<a href="">Read more</a>
+			</div>
+		</div>
+
+		<div class="article-card-read">
+			<h3 class="heyyou-voice">This is the title of an article</h3>
+			<p class="reading-voice">Here's a little info to help you understand if this is an article you want to read.</p>
+
+			<div class="site-button">
+				<a href="">Read more</a>
+			</div>
+		</div>
+
+	</div>
+</article-grid>
