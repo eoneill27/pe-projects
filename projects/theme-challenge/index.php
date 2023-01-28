6f0816f6c93b7f33ad8410797b85065cab8b99c3
Emily <oneill.emily@gmail.com>
2023-01-28 09:05:49 -0800

    Update theme challenge

---
+++ projects/theme-challenge/index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+
+<html lang='en' class='special-magic no-js'>
+
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+		<title>Theme challenge</title>
+		<meta name='description' content='Theme challenge'>
+		<meta property='og:image' content='Can you research emulate a website for a specific niche industry?'>
+
+		<link rel='stylesheet' href='styles/site.css'>
+
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Kreon:wght@300;400;500;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	</head>
+
+
+	<body>
+		<header class="site-header">
+		<inner-column>
+			<?php include('modules/mast-head/template.php'); ?>
+
+		</inner-column>	
+		</header>
+
+		<main>
+
+			<section class='welcome' id='welcome'>
+			<inner-column>
+
+				<?php include('modules/graphic-diptych/template.php'); ?>
+
+			</inner-column>
+			</section>
+
+
+			<section class='sign-up' id='sign-up'>
+			<inner-column>
+
+				<?php include('modules/call-to-action/template.php'); ?>
+
+			</inner-column>
+			</section>
+
+
+			<section class='latest' id='latest'>
+			<inner-column>
+
+				<?php include('modules/articles-intro/template.php'); ?>
+
+			</inner-column>
+			</section>
+
+
+			<section class='get-involved' id='get-involved'>
+			<inner-column>
+
+				<?php include('modules/call-to-action/template.php'); ?>
+
+			</inner-column>
+			</section>
+
+		</main>
+
+		<footer>
+		<inner-column>
+			
+			<?php include('modules/site-map/template.php'); ?>
+
+		</inner-column>
+		</footer>
+	</body>
+
+</html>
