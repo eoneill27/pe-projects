90b77692d59137bedf6bb049ef69e696a1ff1e84
Emily <oneill.emily@gmail.com>
2023-01-29 13:00:54 -0800

    Update About page

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 
 		<section class="about-grid">
 
-			<div class="about-text1">
+			<div class="about-text about-text1">
 				<p class="reading-voice">Born in San Francisco and raised in the wilds of Seattle suburbia, I've made Vancouver, BC my home since 2005.</p>
 
 				<p class="reading-voice">In my spare time, I love to walk long distances, swim in lakes, read in pubs, solve crossword puzzles, play with my friends' kids, and camp in my car.</p>
@@ -26,11 +26,12 @@
 				</picture>
 			</div>
 
-			<div class="about-text2">
-				<p class="reading-voice">My path to web development has been winding. After graduating with a history degree from the University of North Carolina, I made my way to Vancouver for graduate school at the University of British Columbia. I had an interest in archival work, but I ended up getting a degree in both archival and library studies. A combination of luck and circumstance took me away from the archives and into librarianship, and I spent 14 years working as a librarian for the provincial teachers' union. At a certain point, I realized that I needed a change. I wasn't finding my work inspiring or challenging, so I started looking around for something different.</p>
+			<div class="about-text about-text2">
+				<p class="reading-voice">My path to web development has been winding. After graduating with a history degree from the University of North Carolina, I made my way to Vancouver for graduate school at the University of British Columbia. I had an interest in archival work, but I ended up getting a degree in both archival and library studies.</p>
+				<p class="reading-voice">A combination of luck and circumstance took me away from the archives and into librarianship, and I spent 14 years working as a librarian for the provincial teachers' union. At a certain point, I realized that I needed a change. I wasn't finding my work inspiring or challenging, so I started looking around for something different.</p>
 			</div>
 
-			<div class="about-text3">
+			<div class="about-text about-text3">
 				<p class="reading-voice">Enter the mentorship program offered by <a href="https://perpetual.education" target="_blank">Perpetual Education</a>. I was thrilled to start Design for the Web in September, 2022 (while on holiday in Morocco!), and I expect to finish the course in April, 2023.</p>
 
 				<p class="reading-voice">I bring a strong background in information retrieval and organization to my web development work, and I create accessible, responsive websites. I have a strong attention to detail, excellent communication skills, a doggedness when it comes to problem-solving, and a commitment to honest, up-front business dealings. I look forward to meeting you and learning about your project!</p>
