43ba66be1a03993f9cc5292e434a17b7c3282cb1
Emily <oneill.emily@gmail.com>
2023-01-28 20:10:49 -0800

    Image updates

--- projects/theme-challenge/data/cta.php
+++ projects/theme-challenge/data/cta.php
@@ -7,7 +7,7 @@ $ctas = [
 		"paragraph" => "Discover a new way to cook, shop and enjoy food. Taste a world of original meal kit recipes designed by chefs, farm fresh ingredients and top quality groceries! All delivered to your door.",
 		"button" => "Get cooking!",
 		"link" => "#", 
-		"image" => "images/kit.jpg",
+		"image" => "images/kit1.png",
 		"imgClass" => "kit-1",
 		"imgAlt" => "A meal kit with the ingredients arrayed around the box."
 	],
@@ -18,9 +18,9 @@ $ctas = [
 		"paragraph" => "Cooking and cleaing are easy with our pre-cut and pre-portioned ingredients. Spend less time cooking and more time doing what you love.",
 		"button" => "Sign up!",
 		"link" => "#",
-		"image" => "images/family-meal.jpg",
-		"imgClass" => "family-meal",
-		"imgAlt" => "A family eating a meal together."
+		"image" => "images/plate.png",
+		"imgClass" => "plate",
+		"imgAlt" => "A plate of delicious looking food."
 	],
 
 ];
