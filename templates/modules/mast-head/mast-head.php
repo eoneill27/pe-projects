7a8a2ef8c890c2f3b7c927f7b88003bb706ffa6c
Emily <oneill.emily@gmail.com>
2023-02-10 13:15:46 -0800

    Initial commit mast-head module

---
+++ templates/modules/mast-head/mast-head.php
@@ -0,0 +1,11 @@
+<mast-head>
+	<h1 class="logo"><a href="index.php"><?=$logo?></a></h1>
+
+	<nav class="header-nav nav-voice">
+		<a href=<?=$link1?>><?=$link1Title?></a>
+		<a href=<?=$link2?>><?=$link2Title?></a>
+		<a href=<?=$link3?>>
+		<?=$link3Title?></a>
+		<a href=<?=$link4?>><?=$link4Title?></a>
+	</nav>
+</mast-head>
