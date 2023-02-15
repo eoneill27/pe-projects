8667378ff06f317b2537fe59adb83b73031105ae
Emily <oneill.emily@gmail.com>
2023-02-14 21:04:31 -0800

    Update mast-head module

--- templates/modules/mast-head/mast-head.php
+++ templates/modules/mast-head/mast-head.php
@@ -1,8 +1,8 @@
 <mast-head>
-	<h1 class="logo"><a href="index.php"><?=$logo?></a></h1>
+	<h1 class="logo"><a href='<?=$homeLink;?>'><?=$logo?></a></h1>
 
 	<nav class="header-nav nav-voice">
-		<a href=<?=$link1?>><?=$link1Title?></a>
+		<a href=<?="$link1"?>><?=$link1Title?></a>
 		<a href=<?=$link2?>><?=$link2Title?></a>
 		<a href=<?=$link3?>>
 		<?=$link3Title?></a>
