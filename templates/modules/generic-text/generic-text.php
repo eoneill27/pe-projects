5d5fdc97ad1e9ad5a97cb8a2d0f24b47ccc27f48
Emily <oneill.emily@gmail.com>
2023-02-16 22:43:47 -0800

    Update generic-text module with default variables

--- templates/modules/generic-text/generic-text.php
+++ templates/modules/generic-text/generic-text.php
@@ -1,7 +1,14 @@
+<?php 
+
+	$heading = $heading ?? "This is a generic-text module";
+	$text1 = $text1 ?? "This is just some generic text in the generic text module. ";
+	$text2 = $text2 ?? "And more generic text, so you can get a feel for what this looks like. Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Qui distinctio saepe temporibus quisquam ullam necessitatibus voluptatum, rerum minima, quod quae inventore, quis eum officia commodi quo, sequi architecto dolores deserunt?";
+?>
+
 <generic-text>
 
 	<header class="module-header">
-		<h1 class="heyyou-voice"><?=$heading?></h1>
+		<h1 class="attention-voice"><?=$heading?></h1>
 	</header>
 
 	<p class="reading-voice"><?=$text1?></p>
