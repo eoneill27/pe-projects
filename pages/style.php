56413a7038fb30a4321d68f23301eaa450c2447e
Emily <oneill.emily@gmail.com>
2023-02-16 22:41:42 -0800

    Initial commit style guide

---
+++ pages/style.php
@@ -0,0 +1,8 @@
+<inner-column>
+	
+	<?php include('templates/modules/generic-text/generic-text.php'); ?>
+
+	<?php include('templates/modules/graphic-diptych/graphic-diptych.php'); ?>
+
+	<?php include('templates/modules/featured-cards/featured-cards.php'); ?>
+</inner-column>
