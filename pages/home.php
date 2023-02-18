204f2ed7e58156c1deeb9c99fd849d467cad0fd2
Emily <oneill.emily@gmail.com>
2023-02-17 21:34:53 -0800

    Changed variables for generic-text module to make more flexible

--- pages/home.php
+++ pages/home.php
@@ -4,10 +4,8 @@
 
 				<section class="home-welcome">
 					<?php 
-						$heading = "Hello!";
-						$headingClass = "heyyou-voice";
-						$text1 = "In my first career, I spent 15 years working as a professional librarian. In my second career, I hope to focus on helping small businesses build beautiful, functional, accessible web sites. Whether you need a site built from scratch or you have an existing site that needs a bit of TLC, I can help you achieve your goals and build your business.";
-						$text2 = "I draw on my experience as a librarian and my expertise in my web design work to fine-tune the information architecture supporting your website, making your content easily findable and accessible to all of your users.";
+						$textCopy = "<h1 class='heyyou-voice'>Hello!</h1> <p class='reading-voice'>In my first career, I spent 15 years working as a professional librarian. In my second career, I hope to focus on helping small businesses build beautiful, functional, accessible web sites. Whether you need a site built from scratch or you have an existing site that needs a bit of TLC, I can help you achieve your goals and build your business.</p> 
+						<p class='reading-voice'>I draw on my experience as a librarian and my expertise in my web design work to fine-tune the information architecture supporting your website, making your content easily findable and accessible to all of your users.</p>";
 
 						include('templates/modules/generic-text/generic-text.php'); 
 					?>
