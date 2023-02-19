d3873752f9f92deb5708eb2e255e32725b8641c1
Emily <oneill.emily@gmail.com>
2023-02-19 09:00:32 -0800

    Update contact - testing form

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,21 @@
 
 
+<?php
+	// Get data from form
 
+if(isset($_POST['submitted'])) {
+
+$to      = 'oneill.emily@gmail.com';
+$subject = 'Contact form submission';
+$message = $_POST['message'];
+$headers = array(
+    'From' => $_POST['name'],
+    'Reply-To' => $_POST['email'],
+	);
+
+@mail($to, $subject, $message, $headers);
+}
+?>
 
 
 <section class="contact" id="contact">
@@ -42,20 +57,5 @@
 
 
 
-<?php
-	// Get data from form
 
-if(isset($_POST['submitted'])) {
-
-$to      = 'oneill.emily@gmail.com';
-$subject = 'Contact form submission';
-$message = $_POST['message'];
-$headers = array(
-    'From' => $_POST['name'],
-    'Reply-To' => $_POST['email'],
-	);
-
-@mail($to, $subject, $message, $headers);
-}
-?>
 
