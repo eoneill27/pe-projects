1bf90a67616b990198511001fb670e236c53ef87
Emily <oneill.emily@gmail.com>
2023-03-13 20:12:01 -0700

    Update with simple calculator
    Also fixed greeting form to fix problem of output not displaying after form is cleared and another name is entered. Display type was not set in submit event listener.

--- archive/js-tricks/tricks.js
+++ archive/js-tricks/tricks.js
@@ -56,9 +56,9 @@ document.addEventListener('click', function(click) {
 // when the button is clicked, the output appears
 // the output is a string including the input
 
-const form = document.querySelector('form');
+const form = document.querySelector('#hello');
 const nameInput = form.querySelector('#name');
-const output = document.querySelector('output');
+const output = document.querySelector('#hello-output');
 
 const clearButton = document.querySelector('#clearName');
 
@@ -67,6 +67,7 @@ form.addEventListener('submit', function(event) {
 
 	if (nameInput.value) {
 		let message = `Hello ${nameInput.value}, it's nice to meet you!`;
+		output.style.display = "block";
 		output.innerHTML = `<p>${message}</p>`;
 	}	else {
 		ouput.innerHTML = `<p>Please enter a name</p>`;
@@ -81,11 +82,47 @@ clearButton.addEventListener('click', function(clear) {
 });
 
 
+// form - simple calculator
 
+const calculatorForm = document.querySelector('#simple-math');
+const numberOneInput = calculatorForm.querySelector('#number1');
+const numberTwoInput = calculatorForm.querySelector('#number2');
+const mathOutput = document.querySelector('#math-output');
 
+calculatorForm.addEventListener('submit', function(event) {
+	event.preventDefault();
 
+	if (numberOneInput.value && numberTwoInput.value) {
 
+		const numberOne = Number(numberOneInput.value);
+		const numberTwo = Number(numberTwoInput.value);
+		const add = numberOne + numberTwo;
+		const subtract = numberOne - numberTwo;
+		const multiply = numberOne * numberTwo;
+		const divide = numberOne / numberTwo;
 
+		let message1 = `The first number you entered was ${numberOne}.`;
+		let message2 = `The second number you entered was ${numberTwo}.`;
+		let message3 = `${numberOne} + ${numberTwo} = ${add}`;
+		let message4 = `${numberOne} - ${numberTwo} = ${subtract}`;
+		let message5 = `${numberOne} * ${numberTwo} = ${multiply}`;
+		let message6 = `${numberOne} / ${numberTwo} = ${divide}`;
+
+		mathOutput.style.display = "block";
+		mathOutput.innerHTML = "<p>" + message1 + "</p><p>" + message2 + "</p><p>" + message3 + "</p><p>" + message4 + "</p><p>" + message5 + "</p><p>" + message6 + "</p>";
+
+	} else {
+		mathOutput.innerHTML = "<p>Please enter some numbers</p>";
+	}
+});
+
+const clearMath = document.querySelector("#clear-math");
+
+clearMath.addEventListener('click', function(clear) {
+	numberOneInput.value = "";
+	numberTwoInput.value = "";
+	mathOutput.style.display = "none";
+});
 
 
 
