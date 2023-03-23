ee854cf854218e49dac0f82cd9814bad2e2c95b3
Emily <oneill.emily@gmail.com>
2023-03-22 21:43:28 -0700

    Change font styling on home page

--- projects/mockup-sprint/templates/pages/home.php
+++ projects/mockup-sprint/templates/pages/home.php
@@ -1,6 +1,6 @@
 <inner-column>
-	<a href="?page=template&slug=template1">Template 1</a>
-	<a href="?page=template&slug=template2">Template 2</a>
-	<a href="?page=template&slug=template3">Template 3</a>
+	<a class="info-voice" style="margin-top: 20px" href="?page=template&slug=template1">Template 1</a>
+	<a class="info-voice" style="margin-top: 20px" href="?page=template&slug=template2">Template 2</a>
+	<a class="info-voice" style="margin-top: 20px" href="?page=template&slug=template3">Template 3</a>
 </inner-column>
 
