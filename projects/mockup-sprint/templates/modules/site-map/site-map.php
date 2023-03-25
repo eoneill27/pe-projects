59588f6f2336b2e287e44b334caf3a476558298a
Emily <oneill.emily@gmail.com>
2023-03-24 20:33:26 -0700

    Update site-map mobile layout

--- projects/mockup-sprint/templates/modules/site-map/site-map.php
+++ projects/mockup-sprint/templates/modules/site-map/site-map.php
@@ -5,34 +5,36 @@
 	<inner-column>
 
 		<div class="footer-section1">
-			<nav class="product-nav">
-				<h5 class="loud-voice">Product</h5>
-				<ul class="reading-voice">
-					<li>Diam orci</li>
-					<li>Mi feugiat</li>
-					<li>Netus fermentum</li>
-					<li>Suspendisse viverra</li>
-					<li>Id dolor</li>
-					<li>Erat mattis</li>
-				</ul>
-			</nav>
-			<nav class="information-nav">
-				<h5 class="loud-voice">Information</h5>
-				<ul class="reading-voice">
-					<li>Nibh</li>
-					<li>Egestas</li>
-					<li>Dictum</li>
-				</ul>
-			</nav>
-			<nav class="company-nav">
-				<h5 class="loud-voice">Company</h5>
-				<ul class="reading-voice">
-					<li>Id maecenas</li>
-					<li>Id orci</li>
-					<li>Magna ultricies</li>
-					<li>Quis risus</li>
-				</ul>
-			</nav>
+			<div class="nav-menus">
+				<nav class="product-nav">
+					<h5 class="loud-voice">Product</h5>
+					<ul class="reading-voice">
+						<li>Diam orci</li>
+						<li>Mi feugiat</li>
+						<li>Netus fermentum</li>
+						<li>Suspendisse viverra</li>
+						<li>Id dolor</li>
+						<li>Erat mattis</li>
+					</ul>
+				</nav>
+				<nav class="information-nav">
+					<h5 class="loud-voice">Information</h5>
+					<ul class="reading-voice">
+						<li>Nibh</li>
+						<li>Egestas</li>
+						<li>Dictum</li>
+					</ul>
+				</nav>
+				<nav class="company-nav">
+					<h5 class="loud-voice">Company</h5>
+					<ul class="reading-voice">
+						<li>Id maecenas</li>
+						<li>Id orci</li>
+						<li>Magna ultricies</li>
+						<li>Quis risus</li>
+					</ul>
+				</nav>
+			</div>
 
 			<div class="subscribe-box">
 				<h5 class="loud-voice">Subscribe</h5>
